bf54b6dd34f04c0e6946eac18e4713d348a87fdf
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-12 18:56:11 +0300

    commit tree f90af8d32d67c9a27437b846f912498d30fd82de

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+ function findMatching(names,string){
+     let results = [];
+     for(const element of names){
+         if(element === string || element.toLowerCase() === string.toLowerCase())
+         results.push(element)
+     }
+     return results
+
+}
+
+function fuzzyMatch(names,string){
+    let nameSearch = []
+        for(const element of names){
+            if(element[0] === string[0]){
+                nameSearch.push(element)
+
+            }
+        }
+    return nameSearch;
+}
+function matchName(driver, string){
+    let newName = []
+    for(const element of driver){
+        if(element.name === string){
+            newName.push(element)
+        }
+    }
+    return newName
+}
